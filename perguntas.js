977165def66a9d485fa16870d51b0f6da38b8c56
Charlizi <160685331+Charlizi@users.noreply.github.com>
2024-10-08 21:55:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+CriaCartao(
+    'Progamação',
+    'O que é Python?',
+    'O Python é uma linguagem de progamação'
+)
+
+CriaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+CriaCartao(
+    'Progamação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
